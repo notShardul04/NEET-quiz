9301824eb4c4664f39f40231b13aa76e51e3880b
Shardul Satheesh <shardul.sat@gmail.com>
2024-11-04 21:22:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,280 @@
+// script.js
+
+let questions = [
+    {
+        prompt: `The historic convention on 
+                 biological Diversity was held 
+                 in rio de geneiro in the year? `,
+        options: [
+            "1990",
+            "1992",
+            "2002",
+            "2004",
+        ],
+        answer: "1992",
+    },
+    {
+        prompt: `An attribute found in plants but not 
+                 in animals `,
+        options: [
+            "metabolism",
+            "sexual reproduction",
+            "autotrophy",
+            "Asexual reproduction",
+        ],
+        answer: "autotrophy",
+    },
+
+    {
+        prompt: `How many ATP and NADPH2 are 
+                 required for the synthesis of 
+                 one molecule of glucose during
+                 calvin cycle?`,
+        options: [
+            "12 ,12",
+            "18 ,12",
+            "12 ,18",
+            "12 ,16",
+        ],
+        answer: "18 ,12",
+    },
+
+    {
+        prompt: `Identify the heterosporous pteridophytes
+                 from the following`,
+        options: [
+            "selaginella,salvinia",
+            "lycopodium,selaginella",
+            "lycopodium,salvinia",
+            "psilotum,salvinia",
+        ],
+        answer: "selaginella,salvinia",
+    },
+
+    {
+        prompt: `In gene gun method to introduce
+                 alien DNA into the host cell
+                 which metals are used?`,
+        options: ["iron", "copper", "zinc", "Gold or Tungsten"],
+        answer: "Gold or Tungsten",
+    },
+   
+    {
+        prompt: `Which one of hte following sexually transmitted 
+                 disease is curable if detected early?`,
+        options: [
+            "gonorrhoea",
+            "genital herpes",
+            "Hepatitis-B",
+            "HIV infection",
+        ],
+        answer: "gonorrhoea",
+    },
+];
+
+// Get Dom Elements
+
+let questionsEl =
+    document.querySelector(
+        "#questions"
+    );
+let timerEl =
+    document.querySelector("#timer");
+let choicesEl =
+    document.querySelector("#options");
+let submitBtn = document.querySelector(
+    "#submit-score"
+);
+let startBtn =
+    document.querySelector("#start");
+let nameEl =
+    document.querySelector("#name");
+let feedbackEl = document.querySelector(
+    "#feedback"
+);
+let reStartBtn =
+    document.querySelector("#restart");
+
+// Quiz's initial state
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
+
+// Start quiz and hide frontpage
+
+function quizStart() {
+    timerId = setInterval(
+        clockTick,
+        1000
+    );
+    timerEl.textContent = time;
+    let landingScreenEl =
+        document.getElementById(
+            "start-screen"
+        );
+    landingScreenEl.setAttribute(
+        "class",
+        "hide"
+    );
+    questionsEl.removeAttribute(
+        "class"
+    );
+    getQuestion();
+}
+
+// Loop through array of questions and
+// Answers and create list with buttons
+function getQuestion() {
+    let currentQuestion =
+        questions[currentQuestionIndex];
+    let promptEl =
+        document.getElementById(
+            "question-words"
+        );
+    promptEl.textContent =
+        currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(
+        function (choice, i) {
+            let choiceBtn =
+                document.createElement(
+                    "button"
+                );
+            choiceBtn.setAttribute(
+                "value",
+                choice
+            );
+            choiceBtn.textContent =
+                i + 1 + ". " + choice;
+            choiceBtn.onclick =
+                questionClick;
+            choicesEl.appendChild(
+                choiceBtn
+            );
+        }
+    );
+}
+
+// Check for right answers and deduct
+// Time for wrong answer, go to next question
+
+function questionClick() {
+    if (
+        this.value !==
+        questions[currentQuestionIndex]
+            .answer
+    ) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = `Wrong! The correct answer was 
+        ${questions[currentQuestionIndex].answer}.`;
+        feedbackEl.style.color = "red";
+    } else {
+        feedbackEl.textContent =
+            "Correct!";
+        feedbackEl.style.color =
+            "green";
+    }
+    feedbackEl.setAttribute(
+        "class",
+        "feedback"
+    );
+    setTimeout(function () {
+        feedbackEl.setAttribute(
+            "class",
+            "feedback hide"
+        );
+    }, 2000);
+    currentQuestionIndex++;
+    if (
+        currentQuestionIndex ===
+        questions.length
+    ) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+// End quiz by hiding questions,
+// Stop timer and show final score
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl =
+        document.getElementById(
+            "quiz-end"
+        );
+    endScreenEl.removeAttribute(
+        "class"
+    );
+    let finalScoreEl =
+        document.getElementById(
+            "score-final"
+        );
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute(
+        "class",
+        "hide"
+    );
+}
+
+// End quiz if timer reaches 0
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+// Save score in local storage
+// Along with users' name
+
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+        let highscores =
+            JSON.parse(
+                window.localStorage.getItem(
+                    "highscores"
+                )
+            ) || [];
+        let newScore = {
+            score: time,
+            name: name,
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem(
+            "highscores",
+            JSON.stringify(highscores)
+        );
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startBtn.onclick = quizStart;
